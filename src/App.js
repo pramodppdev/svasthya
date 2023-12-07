3d43390d9051b1ada56391aafd7997c1d4c3e782
thealchimests <thealchemists.98@gmail.com>
2023-12-07 20:52:07 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import TopNavbar from './components/home/nav';
-=======
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react';
-import Dashboard from './components/admin/Dashboard';
-
->>>>>>> 0442e9fc1e744a9f0fe13bec22368e1a52b31465
 function App() {
   return (
     <Router>
