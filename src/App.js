0442e9fc1e744a9f0fe13bec22368e1a52b31465
grp-03 <grpdev.03@gmail.com>
2023-12-07 20:36:36 +0530

    raghu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Main from './components/home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import Dashboard from './components/admin/Dashboard';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/" component={Dashboard}></Route>
          
         </Switch>
       </div>
