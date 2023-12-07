cea3c8b9f5c0c6b28fbc589a7afb2348b601ef28
thealchimests <thealchemists.98@gmail.com>
2023-12-07 20:53:11 +0530

    jjs

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import TopNavbar from './components/home/nav';
+import React from 'react';
+import Dashboard from './components/admin/Dashboard';
+
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-<<<<<<< HEAD
           <Route exact path="/" component={TopNavbar}></Route>
+          <Route exact path="/dashboard" component={Dashboard}></Route>
           
 
-=======
-          <Route exact path="/" component={Dashboard}></Route>
->>>>>>> 0442e9fc1e744a9f0fe13bec22368e1a52b31465
          
         </Switch>
       </div>
