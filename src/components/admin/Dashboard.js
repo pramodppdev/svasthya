12eb0f9f9b1298030a9168c64f60e338466be11f
grp-03 <grpdev.03@gmail.com>
2023-12-07 20:53:42 +0530

    raghu

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -8,7 +8,13 @@ const Dashboard = () => {
     <div className="dashboard-container">
       <Sidebar />
       <main class="main-content">
-      
+      <div class="cards">
+        <div class="card">
+          <div class="name"><h3>Blood donated</h3></div>
+          <div class="num"><h3>100</h3></div>
+        </div>
+
+      </div>
       </main>
     </div>
   );
