75ceeb5cf92ec3b52884133848824ec1dd260348
grp-03 <grpdev.03@gmail.com>
2023-12-08 07:31:52 +0530

    raghu commited

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import TopNavbar from './components/home/nav';
-import React from 'react';
 import Dashboard from './components/admin/Dashboard';
+import Blood from './components/admin/Blood';
+import Form from './components/admin/requested';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={TopNavbar}></Route>
-          <Route exact path="/dashboard" component={Dashboard}></Route>
-          
+{/*<!--<Route exact path="/" component={TopNavbar}></Route>--!>*/}
+          <Route exact path="/" component={Dashboard}></Route>
+          <Route exact path="/blood" component={Blood}></Route>
+          <Route exact path="/Form" component={Form}></Route>
 
          
         </Switch>
